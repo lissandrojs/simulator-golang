c01707abf223fbeca2864c9dc195140fbae40039
Lissandro kenziano <lissandromr@hotmail.com>
2023-02-11 13:59:09 -0300

    feat: estruturando projeto golang

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/joho/godotenv"
+	"github.com/lissandrojs/simulator-golang/application/route"
+)
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("error loading .env file")
+	}
+}
+
+func main() {
+
+	route := route.Route{
+		ID:       "1",
+		ClientID: "1",
+	}
+
+	route.LoadPositions()
+	stringjson, _ := route.ExportJsonPositions()
+	fmt.Println(stringjson[0])
+}
